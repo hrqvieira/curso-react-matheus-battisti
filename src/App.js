f8672730d08cec8cb993a5a1f3a0103003b5a771
Henrique Vieira <vieira.henrique02@outlook.com>
2023-01-03 20:30:15 -0300

    avançando em props

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
     <div className="App">
       <h1>Testando eventos</h1>
       <Evento numero="1"/>
-      <Evento numero="2"/>
+
       <Form />
     </div>
   );
--- src/components/Evento.js
+++ src/components/Evento.js
@@ -1,19 +1,23 @@
+import Button from "./evento/Button";
 
+function Evento() {
+  function meuEvento1() {
+    console.log(`Ativando primeiro evento`);
+  }
 
-function Evento({numero}){
+  function segundoEvento(){
+    console.log("Ativando segundo elemento")
+  }
 
-    function meuEvento() {
-        console.log(`Opa, fui ativado! ${numero}`)
-    }
-
-
-    return(
+  return (
     <div>
-        <p>Clique para disparar um evento</p>
-        <button onClick={meuEvento}>Ativar!</button>
-    </div>        
-)
+      <p>Clique para disparar um evento</p>
+      <Button event={meuEvento1} text="Primeiro Evento" />
+
+      <p>Clique para disparar um evento</p>
+      <Button event={segundoEvento} text="Segindo Evento" />
+    </div>
+  );
 }
 
-
-export default Evento
+export default Evento;
